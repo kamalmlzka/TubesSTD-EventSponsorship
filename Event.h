326934b50da0b6c99473dcaa3ba2e2f04ed9d38d
Kamal Maulaazka Sidhqi <kamalmlzka@student.telkomuniversity.ac.id>
2022-12-21 18:51:41 +0700

    Update Event.h

--- Event.h
+++ Event.h
@@ -1,1 +1,42 @@
+#include <iostream>
+#include "Eventlib.h"
+#include "Sponsorlib.h"
 
+#define info(P) P->info
+#define next(P) P->next
+#define prev(P) P->prev
+#define Event(P) P->Event
+#define Sponsor(P) P->Sponsor
+#define first(L) L.first
+#define nil NULL
+
+using namespace std;
+
+struct relasi {
+    string dukungan;
+    int budgetDukungan;
+};
+
+typedef struct elmRelasi *adr_Relasi;
+
+typedef relasi infotype;
+
+struct elmRelasi {
+    infotype info;
+    adr_Relasi next;
+    adr_Relasi prev;
+    adr_Sponsor Sponsor;
+    adr_Event Event;
+};
+
+struct ListRelasi {
+    adr_Relasi first;
+};
+
+void createListRelasi(ListRelasi &L,);
+adr_Relasi newRelasi(adr_Event E, adr_Sponsor S, infotype x);
+void insertLastRelasi(ListRelasi &L, adr_Relasi P);
+void deleteFirstRelasi(ListRelasi &L, adr_Relasi &P);
+adr_Relasi cariRelasi(ListRelasi L, adr_Event E, adr_Sponsor S);
+void showRelasi(ListRelasi L);
+void hapusRelasi(ListRelasi &L, adr_Relasi &P);
